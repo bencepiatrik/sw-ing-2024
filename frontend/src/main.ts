ec5074eaa828d4419e790631477c68cafccc3e64
Zilizi Márk <zilizimark@gmail.com>
2024-12-03 21:04:06 +0100

    Vuetify added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import {useAuthStore} from "@/stores/authStore";
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
 
+const vuetify = createVuetify();
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify);
 
 const authStore = useAuthStore();
 authStore.initialize(); // Initialize authentication state
