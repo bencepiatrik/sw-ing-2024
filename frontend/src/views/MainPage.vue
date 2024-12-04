be23314b8836952f986373566834e9c2a81b94e6
Balázs Forró <forrobalazs01@gmail.com>
2024-12-04 02:12:32 +0100

    MainPage to Profile button

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -3,6 +3,7 @@
     <v-app-bar app>
       <v-toolbar-title>Main Page</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn text href="/profile">Profile</v-btn>
       <v-btn text href="/">Landing</v-btn>
       <v-btn text @click="handleLogout">Logout</v-btn>
     </v-app-bar>
