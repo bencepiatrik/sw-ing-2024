a7ccd0b1c6d9b29dc64bd7bde03700b914c67e76
darci <pinter.darius@gmail.com>
2024-12-17 17:54:34 +0100

    fixed migrations and seeders

--- backend/database/migrations/2024_11_03_232201_create_categories_table.php
+++ backend/database/migrations/2024_11_03_232201_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 32)->unique();
+            $table->text('description')->nullable();
         });
     }
 
